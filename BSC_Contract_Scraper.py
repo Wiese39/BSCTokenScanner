87a928a8a80a35d081adaa0a7c21c55ee5da1e0f
Wiese39 <wiese.jeremy@gmail.com>
2023-04-09 16:17:19 +0200

    make it multichain wooho

--- BSC_Contract_Scraper.py
+++ BSC_Contract_Scraper.py
@@ -7,88 +7,112 @@ from webdriver_manager.chrome import ChromeDriverManager
 import re
 
 
-def unique():
-    with open("addresses.txt", "r") as f:
+def unique(file):
+    with open(f"{file}.txt", "r") as f:
         addresses = f.read().splitlines()
         addresses = list(dict.fromkeys(addresses))
         with open("addresses.txt", "w") as f:
             for address in addresses:
                 f.write(address + "\n")
-# unique()
-# exit()
 
-driver = Chrome() # Replace with path to your chromedriver executable
-#use subprocess
-driver.get('https://etherscan.com/login') # Navigate to the login page
-time.sleep(20) # Wait for the page to load and for user to complete captcha
-
-#auto login
-# driver.find_element_by_id("login").send_keys("wiese222")
-# driver.find_element_by_id("password").send_keys("Hallo123!")
-# driver.find_element_by_id("loginbtn").click()
-# time.sleep(5)
-add_url = "?subcatid=undefined&size=50&start=0&col=3&order=desc"
 start = 0
 page = 1
 count = 0
-url = 'https://etherscan.com/labelcloud'
 keyword = 'tokens/label'
 links = []
 
-# # Load the page
-# driver.get(url)
+def get_links(url, keywords):
+    driver.get(url) # Navigate to the page
+    while "Ray ID:" in driver.page_source:
+        # Wait for 7 seconds to allow Cloudflare to verify the request
+        time.sleep(0.3)
+    time.sleep(1.3) # Wait for the page to load
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    response = soup.find_all('a')
+    for a in response:
+        for keyword in keywords:
+            if keyword in a['href']:
+                links.append(a['href'])
+    with open("links.txt", "w") as f:
+        for link in links:
+            f.write(link + "\n")
 
-# # Find all links that contain the keyword
-# elements = driver.find_elements(By.XPATH, f"//a[contains(@href, '{keyword}')]")
+def scrape_contracts(url, add_url, output_file):
+    #strip "labelcloud" from the url
+    url = url.split("/labelcloud")[0]
+    driver.get(f"{url}/login") # Navigate to the login page
+    time.sleep(20) # Wait for the page to load and for user to complete captcha
+    with open("links.txt", "r") as f:
+        links = f.read().splitlines()
+        for link in links:
+            URL = link + add_url
+            print("Scraping url " + URL)
+            while True:
+                driver.get(URL) # Navigate to the page
+                while "Ray ID:" in driver.page_source:
+                    # Wait for 7 seconds to allow Cloudflare to verify the request
+                    time.sleep(0.3)
+                time.sleep(1.3) # Wait for the page to load
+                soup = BeautifulSoup(driver.page_source, 'html.parser')
+                response = soup.find_all('td')
+                td_regex = r"<td[^>]*>(.*?)<\/td>"
+                td_matches = re.findall(td_regex, str(response))
+                for td in td_matches:
+                    if "Contract" in td:
+                        match = re.search(r'href="([^"]+)"', td)
+                        if match:
+                            href = match.group(1)
+                            address = href.split("/address/")[1]
+                            with open(f"{output_file}.txt", "a") as f:
+                                f.write(address + "\n")
+                                count += 1
+                if "tokens" in URL:
+                    #find all href in the page
+                    hrefs = re.findall(r'href="([^"]+)"', str(response))
+                    for href in hrefs:
+                        if "address" in href:
+                            address = href.split("/address/")[1]
+                            with open(f"{output_file}.txt", "a") as f:
+                                f.write(address + "\n")
+                                count += 1
+                #if this page is loaded https://bscscan.com/error then break
+                if "error" in driver.current_url:
+                    print("Scraped " + str(count) + " addresses on " + str(page) + " pages.")
+                    if page > 1:
+                        page -= 1
+                    break
+                start += 50
+                page += 1
+                #replace start= with start={start}
+                URL = URL.replace(f"start={start-50}", f"start={start}")
 
-# # Extract the links from the elements
-# links = [element.get_attribute('href') for element in elements]
-# #append all links to file
-# with open("links.txt", "a") as f:
-#     for link in links:
-#         f.write(link + "\n")
+    with open(f"{output_file}.txt", "r") as f:
+        addresses = f.read().splitlines()
+        print("Total number of addresses: " + str(len(addresses))) 
+    driver.quit() # Close the browser window when done
 
-#loop through all links from file
-with open("links.txt", "r") as f:
-    links = f.read().splitlines()
-    for link in links:
-        URL = link + add_url
-        print("Scraping url " + URL)
-        driver.get(URL) # Navigate to the page
-        while "Ray ID:" in driver.page_source:
-            # Wait for 7 seconds to allow Cloudflare to verify the request
-            time.sleep(0.3)
-        time.sleep(1.3) # Wait for the page to load
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        response = soup.find_all('td')
-        td_regex = r"<td[^>]*>(.*?)<\/td>"
-        td_matches = re.findall(td_regex, str(response))
-        for td in td_matches:
-            if "Contract" in td:
-                match = re.search(r'href="([^"]+)"', td)
-                if match:
-                    href = match.group(1)
-                    address = href.split("/address/")[1]
-                    with open("addresses.txt", "a") as f:
-                        f.write(address + "\n")
-                        count += 1
-        
-        #if this page is loaded https://bscscan.com/error then break
-        if "error" in driver.current_url:
-            print("Scraped " + str(count) + " addresses on " + str(page) + " pages.")
-            if page > 1:
-                page -= 1
-            continue
-        start += 50
-        page += 1
-        #replace start= with start={start}
-        URL = URL.replace(f"start={start-50}", f"start={start}")
-
-with open("addresses.txt", "r") as f:
-    addresses = f.read().splitlines()
-    print("Total number of addresses: " + str(len(addresses))) 
-driver.quit() # Close the browser window when done
-
-
-
-
+if __name__ == "__main__":
+    driver = Chrome() # Replace with path to your chromedriver executable
+    #use subprocess
+    add_url = "?subcatid=undefined&size=50&start=0&col=3&order=desc"
+    chain = input("Enter the name of the chain you want to scrape (e.g. bsc, ether, polygonscan, avax): ")
+    links = input("Extract links from the labelcloud page? (y/n): ")
+    file = input("Enter the name of the output file: ")
+    linkgetter = False
+    keyword = []
+    if links == "y":
+        linkgetter = True
+    if linkgetter:
+        if not chain == "avax":
+            url = f"https://{chain}scan.com/labelcloud"
+            get_links(url, keyword)
+        else:
+            url = f"https://snowtrace.io/labelcloud"
+            get_links(url, keyword)
+    if not chain == "avax":
+        url = f"https://{chain}scan.com/labelcloud"
+        scrape_contracts(url, add_url, chain)
+    else:
+        url = f"https://snowtrace.io/labelcloud"
+        scrape_contracts(url, add_url, chain)
+    unique(file)
